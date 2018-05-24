0edd25930e0130789f393995cfacefcf585da0b0
xiaohaiH <773884562@qq.com>
2018-05-24 19:45:13 +0800

    啊啊啊啊啊啊

--- a.sh
+++ a.sh
@@ -1,1 +1,1 @@
-﻿git add . && git commit -am"啊啊啊啊啊啊" && git push origin master||git checkout sss && git merge master && git push origin sss && git checkout master
+﻿git add . && git commit -am"啊啊啊啊啊啊" && git push origin master;git checkout sss && git merge master && git push origin sss && git checkout master
